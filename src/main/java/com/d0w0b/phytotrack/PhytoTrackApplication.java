dc69097300c782fa820012d7956429a1d3499696
qo0u0op <vikrolinne07@outlook.com>
2026-05-31 17:48:19 +0800

    chore: initialize Spring Boot project

---
+++ src/main/java/com/d0w0b/phytotrack/PhytoTrackApplication.java
@@ -0,0 +1,13 @@
+package com.d0w0b.phytotrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhytoTrackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PhytoTrackApplication.class, args);
+  }
+
+}
